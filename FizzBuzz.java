7f2e68994ddfee177019953d782ef1949b00b1db
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-01-18 18:38:52 -0500

    Added FizzBuzz  to repo

---
+++ FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+	public static void main(String args[]) {
+		final int n = 100;
+		for(int i = 0; i < n; i++) {
+			if(((i % 5) == 0) &&
+				((i%3) == 0)) {
+				System.out.println("FizzBuzz");
+			}
+			else if((i % 3) == 0) {
+				System.out.println("Fizz");
+			}
+			else if((i % 5) == 0) {
+				System.out.println("Buzz");
+			}
+		}
+	}
+}
